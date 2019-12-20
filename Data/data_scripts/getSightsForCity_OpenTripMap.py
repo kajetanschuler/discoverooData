628941f2e3cbf9c0b572423eb6f3329828615377
Kajetan Schuler <kajetan.schuler@gmail.com>
2019-12-20 21:47:27 +0100

    Started working on OpenTripMap Script

--- Data/data_scripts/getSightsForCity_OpenTripMap.py
+++ Data/data_scripts/getSightsForCity_OpenTripMap.py
@@ -1,118 +1,118 @@
+import csv
 import requests
 import json
-import time
-import csv
 
 
 def main():
+    # Open .csv file that contains all cities with a minimum population of 200.000
+    with open ('../data_processed/allCitiesMinPopulation.csv', 'rt') as input:
+        reader = csv.reader(input)
 
-    country_list = ["IT", "ES", "TH", "IN", "SE", "DK", "BR", "AR", "CA", "ZA", "EG", "JP", "AU", "PL", "ME"]
-
-    for country in country_list:
-        more_data = True
-        total_count = 0
-        count = 0
-        offset = 0
-        csv_file_name = "cities" + country + ".csv"
-        with open(csv_file_name, mode='a') as file:
-            z = csv.reader(file, delimiter='\t')
-
-        with open(csv_file_name, mode='w') as csv_file:
-            fieldnames = ['countryCode', 'type', 'cityName', 'cityId', 'regionName', 'regionCode', 'lat', 'lon', 'population', 'elevation', 'timezone']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        # Open/Create csv file for historic sights data
+        with open ('../data_raw/historicalSightsInCities.csv', 'wt') as output:
+            fieldnames = ['cityId', 'searchRadius',
+                          'hCountLevel0', 'hCountLevel1', 'hCountLevel2', 'hCountLevel3', 'hCountLevel7',
+                          'cCountLevel0', 'cCountLevel1', ]
+            writer = csv.DictWriter(output, fieldnames=fieldnames)
             writer.writeheader()
 
-            try:
+            # Skip header when reading csv input file
+            next(reader, None)
 
-                while more_data:
-                    url = "https://wft-geo-db.p.mashape.com/v1/geo/cities"
-                    querystring = {"limit": "100", "countryIds": country, "offset": offset}
-                    headers = {
-                        'x-rapidapi-host': "wft-geo-db.p.rapidapi.com",
-                        'x-rapidapi-key': "e1f6061817mshb6a5d107db1f20fp1b58cejsn545de208fcf6"
-                    }
-                    response = requests.request("GET", url, headers=headers, params=querystring)
-                    data = json.loads(response.text)
+            # Iterate through all rows of the .csv input file and get necessary data
+            for row in reader:
+                city_id = row[3]
+                population = row[8]
+                lat = row[6]
+                lon = row[7]
 
-                    if total_count == 0:
-                        total_count = data['metadata']['totalCount']
+                url = url_builder(lat, lon, population, "historic")
+                response = requests.request("GET", url)
 
-                    country_code = ""
-                    city_name = ""
-                    city_id = ""
-                    region_name = ""
-                    region_code = ""
-                    latitude = ""
-                    longitude = ""
-                    type = ""
+                data = json.loads(response.text)
 
-                    if 'data' in data:
+                skip = False
 
-                        for x in data['data']:
+                rate = ""
+                wiki_id = ""
+                xid = ""
+                osm = ""
+                h_count_level0 = 0
+                h_count_level1 = 0
+                h_count_level2 = 0
+                h_count_level3 = 0
+                h_count_level7 = 0
 
-                            if 'countryCode' in x:
-                                country_code = x['countryCode']
 
-                            if 'name' in x:
-                                city_name = x['name']
+                wiki_id_list = []
+                xid_list = []
+                osm_list = []
 
-                            if 'id' in x:
-                                city_id = x['id']
+                for x in data['features']:
+                    characteristics = x['properties']
+                    if 'rate' in characteristics:
+                        rate = characteristics['rate']
 
-                            if 'region' in x:
-                                region_name = x['region']
+                    if 'wikidata' in characteristics:
+                        wiki_id = characteristics['wikidata']
+                        if wiki_id in wiki_id_list:
+                            skip = True
+                        else:
+                            wiki_id_list.append(wiki_id)
 
-                            if 'regionCode' in x:
-                                region_code = x['regionCode']
+                    if 'xid' in characteristics:
+                        xid = characteristics['xid']
+                        if xid in xid_list:
+                            skip = True
+                        else:
+                            xid_list.append(xid)
 
-                            if 'latitude' in x:
-                                latitude = x['latitude']
+                    if 'osm' in characteristics:
+                        osm = characteristics['osm']
+                        if osm in osm_list:
+                            skip = True
+                        else:
+                            osm_list.append(osm)
 
-                            if 'longitude' in x:
-                                longitude = x['longitude']
+                    if not skip:
+                        if rate >= 7:
+                            h_count_level7 += 1
+                        elif rate >= 3:
+                            h_count_level3 += 1
+                        elif rate >= 2:
+                            h_count_level2 += 1
+                        elif rate >= 1:
+                            h_count_level1 += 1
+                        elif rate == 0:
+                            h_count_level0 += 1
 
-                            if 'type' in x:
-                                type = x['type']
+# Todo: Reset list variables to empty
+# Todo: Run next request with same city but different "kind"
+# Todo: Write Full row into csv
+# Todo: Run next row with new City Id
 
-                            count += 1
 
-                            url = "https://wft-geo-db.p.mashape.com/v1/geo/cities/" + str(city_id)
-                            headers = {
-                                'x-rapidapi-host': "wft-geo-db.p.rapidapi.com",
-                                'x-rapidapi-key': "e1f6061817mshb6a5d107db1f20fp1b58cejsn545de208fcf6"
-                            }
-                            response = requests.request("GET", url, headers=headers, params=querystring)
-                            pop_data = json.loads(response.text)
-                            pop_data = pop_data['data']
+# Funtion to build the request url
+def url_builder(lat, lon, population, kinds):
+    api_key = "&apikey=5ae2e3f221c38a28845f05b6cb6b4ac567a0e6b3fcde2740c98bc367"
+    base_url = "https://api.opentripmap.com/0.1/en/places/"
+    if int(population) > 1000000:
+        radius = 50000
 
-                            population = ""
-                            elevation = ""
-                            timezone = ""
+    elif int(population) > 500000:
+        radius = 30000
 
-                            if 'population' in pop_data:
-                                population = pop_data['population']
+    else:
+        radius = 20000
 
-                            if 'elevationMeters' in pop_data:
-                                elevation = pop_data['elevationMeters']
+    radius = "radius?radius=" + str(radius)
+    lat = "&lat=" + str(lat)
+    lon = "&lon=" + str(lon)
+    kinds = "&kinds=" +  kinds
+    limit = "&limit=" + str(10000)
 
-                            if 'timezone' in pop_data:
-                                timezone = pop_data['timezone']
-
-                            writer.writerow({'countryCode': country_code, 'type': type, 'cityName': city_name, 'cityId': city_id,
-                                             'regionName': region_name, 'regionCode': region_code, 'lat': latitude,
-                                             'lon': longitude, 'population': population, 'elevation': elevation, 'timezone': timezone})
-
-                            # time.sleep(0.1)
-
-                    print(str(count) + " of " + str(total_count))
-                    offset += 100
-
-                    if count == total_count:
-                        more_data = False
-                        print( csv_file_name + ": Completed!")
-
-            except requests.exceptions.RequestException as e:
-                print(e)
+    url = base_url + radius + lon + lat + kinds + limit + api_key
+    return url
 
 
 if __name__ == '__main__':
