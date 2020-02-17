bef9350a8e53c22713d213b126a77706b08bf144
Malik Caycioglu <caycioglu.malik@gmail.com>
2020-02-17 18:51:08 +0100

    Das Skript ändert die ISO3 Codes der Ursprungsdatei in ISO2 Codes und droppt NaN Werte

---
+++ python/data_processing_scripts/mergeCostAndQuality.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def main():
+
+    costData = pd.read_csv("../data_raw/costAndSafetyDataForCountry.csv")
+    infrastructureData = pd.read_csv("../data_processed/qualityOfInfrastructure_clean.csv")
+
+    merge = pd.merge(left=costData, right=infrastructureData, on="countryCode")
+
+    header = ['countryCode', 'CountryName', 'Value1718', 'cpiRentIndex', 'cpiIndex', 'groceriesIndex',
+              'purchasingPowerIndex', 'restaurantIndex', 'rentIndex', 'safetyIndex', 'crimeIndex']
+
+    merge.to_csv("../data_processed/costAndQuality_clean.csv", columns=header, index=False)
+
+    print("Merge complete!")
+    print(merge)
+
+
+if __name__ == '__main__':
+    main()
