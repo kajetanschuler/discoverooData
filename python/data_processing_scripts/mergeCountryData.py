33a3865ab9383220cdc3e24f29af40ff1fd919ef
kajetanschuler <kajetan.schuler@gmail.com>
2020-02-26 19:07:41 +0100

    Final city data added

--- python/data_processing_scripts/mergeCountryData.py
+++ python/data_processing_scripts/mergeCountryData.py
@@ -9,7 +9,7 @@ def main():
     infrastructure_data = infrastructure_data.drop('countryName', axis=1)
     country_data = pd.read_csv("../data_raw/countryData.csv")
 
-    merge1 = pd.merge(left=cost_data, right=infrastructure_data, on="countryCode")
+    merge1 = pd.merge(left=infrastructure_data, right=cost_data, on="countryCode")
 
     merge2 = pd.merge(left=merge1, right=country_data, on="countryCode")
 
