bc0e39d50d4bad93b7336006eb36e5b28657adbd
Malik <caycioglu.malik@gmail.com>
2020-03-17 00:17:03 +0100

    Links zu den Bildern bei city_data_final hinzugefügt (city_Id + jpg)

--- python/data_processing_scripts/addImagePathToCityData.py
+++ python/data_processing_scripts/addImagePathToCityData.py
@@ -8,9 +8,8 @@ def main():
     city_data['image_links'] = city_data['cityId'].apply(lambda x: str(x) + ".jpg")
     print(city_data)
 
-    city_data.to_csv("../data_final/city_data_final_image_links", index=False)
+    city_data.to_csv("../data_final/city_data_final_image_links.csv", index=False)
 
-    #city_data_image.to_csv("../data_processed/cultural_indices.csv", columns=header, index=False)
 
 
 if __name__ == '__main__':
