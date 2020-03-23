8382ef00b3a744554cff9f226717bd7faea42480
Malik <caycioglu.malik@gmail.com>
2020-03-23 12:32:26 +0100

    Links zu den Bildern bei city_data_final hinzugefügt (city_Id + jpg)

--- python/data_processing_scripts/addImagePathToCityData.py
+++ python/data_processing_scripts/addImagePathToCityData.py
@@ -1,3 +1,6 @@
+# Skript, das eine "image_links" Spalte zu city_data_final hinzufügt um auf das jeweilige Stadtbild zu verweisen
+# created 20.03.2020 by Malik
+
 import pandas as pd
 import numpy as np
 
